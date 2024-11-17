b0e0bfcedb29497c03a74f9e0c547a9b0735ca0c
aadarsh <aadarshg087@gmail.com>
2024-11-17 13:49:04 +0530

    Creater Utils

--- src/index.js
+++ src/index.js
@@ -6,4 +6,12 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./databases/index.js";
 
-connectDB();
+connectDB()
+  .then(() => {
+    application.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo connection failed :", err);
+  });
