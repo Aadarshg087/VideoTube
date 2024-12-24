58e822b00590699443f8bb5d5ae5e022f72f0cb5
aadarsh <aadarshg087@gmail.com>
2024-12-24 21:38:49 +0530

    Added authication middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -171,10 +171,28 @@ const loginUser = asyncHandler(async (req, res) => {
 STEP TO LOGOUT THE USER :
 - Find the _id or email or username of the current loggedin user
 - We can do that using middleware using jwt tokens
+- 
 - Then, we can import the user data here and clear refreshToken from the database
 
 */
 
-const logoutUser = asyncHandler(async (req, res) => {});
+const logoutUser = asyncHandler(async (req, res) => {
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    { $set: { refreshToken: undefined } },
+    { new: true }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
+});
 
 export { registerUser, loginUser, logoutUser };
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,1 +1,25 @@
-import { JsonWebTokenError } from "jsonwebtoken";
+import jwt, { decode } from "jsonwebtoken";
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import { User } from "../models/user.model";
+
+const verifyJWT = asyncHandler(async (req, _) => {
+  const token = req.cookies?.accessToken;
+
+  if (!token) {
+    throw new ApiError(401, "Unauthrozise access");
+  }
+
+  const decodedInfo = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+  const user = await User.findById(decodedInfo._id).select(
+    "-password -refreshToken"
+  );
+  if (!user) {
+    throw new ApiError(401, "Invalid access token");
+  }
+  req.user = user;
+  next();
+});
+
+export { verifyJWT };
